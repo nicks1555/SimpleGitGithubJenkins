e6bc11607721e0feedad69bec78e670ba4a31572
nicks1555 <nicks1555@gmail.com>
2023-05-27 12:31:22 +0530

    updated

--- src/simpleJava/SimplePrint.java
+++ src/simpleJava/SimplePrint.java
@@ -3,7 +3,34 @@ package simpleJava;
 public class SimplePrint {
 
 	public static void main(String[] args) {
-		System.out.println("Hello .......");
+		int n=5;
+		
+
+		for(int i=1;i<=n;i++)
+		{   System.out.print("\n");
+			for(int k=n-i;k>=1;k--)
+			{	
+				System.out.print(" ");
+			}
+			
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print("* ");
+			}
+		}
+		for(int i=1;i<=n-1;i++)
+		{   System.out.print("\n");
+			for(int k=1;k<=i;k++)
+			{	
+				System.out.print(" ");
+			}
+			
+			for(int j=n-1;j>=i;j--)
+			{
+				System.out.print("* ");
+			}
+		
+		}
 
 	}
 
